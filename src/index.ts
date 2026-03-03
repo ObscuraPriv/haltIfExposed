d53cf71c9fec4e4f8a8c246ad574ee8c193dffb0
ObscuraPriv <baseclawpolice@gmail.com>
2026-03-02 23:07:25 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 export function enforce() {
   console.log("Obscura deterministic enforcement layer initialized.")
 }
+
+export * from "./types";
+export * from "./policy";
+export * from "./state-machine";
+export * from "./attestation";
+export * from "./enforcement";
